89f3141f6646125e88d9b3c9999ff4e3c693aaf1
yesukay2 <yesukay2@gmail.com>
2022-11-03 14:42:35 +0000

    finished design for desktop view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
-var thumbnail = document.getElementById("thumb1")
+var thumbnail1 = document.getElementById("thumb1")
+var thumbnail2 = document.getElementById("thumb2")
+var thumbnail3 = document.getElementById("thumb3")
+var thumbnail4 = document.getElementById("thumb4")
 
-console.log(thumbnail);
-function showImage(src) {
-    thumbnail.src = src;
+
+function showThumb1(src) {
+    thumbnail1.src = src;
 }
+function showThumb2(src) {
+    thumbnail2.src = src;
+}
+function showThumb3(src) {
+    thumbnail3.src = src;
+}
+function showThumb4(src) {
+    thumbnail4.src = src;
+}
+
+
+// var header = document.querySelector(".header-bottom");
+// if (document.body.scrollBy > 1) {
+//     header.setAttribute("id", "sticky");
+// } else {
+//     header.setAttribute('id', "")
+// }
+// console.log(header);
+
+
+// $(document).ready(function(){
+//   if (document.body.scrollBy > 1) {
+//     header.setAttribute("id", "sticky");
+// } else {
+//     header.setAttribute('id', "")
+// }
+// });
+
+const scrollVal = document.querySelector(".scrollable");
+
+scrollVal.addEventListener("scroll", () => {
+    console.log(`scroll value is: ${scrollVal.scrollTop}`);
+    console.log(scrollVal);
+})
